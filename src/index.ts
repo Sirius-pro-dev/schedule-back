f8d9f853bdb5b1383b662cf045f2b833f84eed48
Limonadek <den-stepanov-03@mail.ru>
2023-09-20 17:26:01 +0300

    fix docker-compose

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.APP_PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('hello');
 })
 
-app.listen(4000, () => {
-  console.log(`server running on port 4000`);
-});
+const start = async () => {
+  try {
+      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (e) {
+      console.log(e);
+  }
+}
+
+start();
