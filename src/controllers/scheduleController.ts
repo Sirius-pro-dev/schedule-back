ecfb795fd6cdf4c347d0e778384317fabb35d15b
dancinginthedarkk <91607846+dancinginthedarkk@users.noreply.github.com>
2023-10-14 10:32:12 +0300

    fix: fixing controllers (#9)
    * fix: fixing controller create
    
    * fix: fixing controller create

--- src/controllers/scheduleController.ts
+++ src/controllers/scheduleController.ts
@@ -75,7 +75,7 @@ class ScheduleController {
         return;
       }
 
-      const userData = await User.find({ _id: { $in: users } });
+      const userData = await User.find({ userName: { $in: users } });
       if (userData.length !== users.length) {
         next(
           ApiError.notFound('Не найден пользователь', 'scheduleController/createSchedule')
